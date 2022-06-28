79eaf0e41657cf2fef1885d9a4413cda8a399299
takuya morishita <102013434+takushi53@users.noreply.github.com>
2022-06-28 22:01:30 +0900

    課題再提出

--- src/Chapter5.java
+++ src/Chapter5.java
@@ -1,23 +1,30 @@
-import java.time.LocalDate;
 import java.io.IOException;
-import java.util.TreeMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Chapter5 {
 	public static void main(String[] args) throws IOException {
 		Task task = new Task();
-		TreeMap<LocalDate, String> taskList = new TreeMap<LocalDate, String>();
-		taskList.put(task.day1, task.task1);
-		taskList.put(task.day2, task.task2);
-		taskList.put(task.day3, task.task3);
-		taskList.put(task.day4, task.task4);
-		taskList.put(task.day5, task.task5);
 		
-		for (LocalDate days : taskList.keySet()) {
-			System.out.println(days + "：" + taskList.get(days));
-			
+		Task task1 = new Task(task.task1, task.day1);
+		Task task2 = new Task(task.task2, task.day2);
+		Task task3 = new Task(task.task3, task.day3);
+		Task task4 = new Task(task.task4, task.day4);
+		Task task5 = new Task(task.task5, task.day5);
+				
+		List<Task> taskList = new ArrayList<>();
+		taskList.add(task1);
+		taskList.add(task2);
+		taskList.add(task3);
+		taskList.add(task4);
+		taskList.add(task5);
+		
+		
+		Collections.sort(taskList);
+		for (int i = 0; i < taskList.size(); i++) {
+			System.out.println(taskList.get(i).getDay() + ":" + taskList.get(i).getTask());
 		}
-		
-		
-		
+
 	}
 }
--- src/Task.java
+++ src/Task.java
@@ -1,16 +1,48 @@
 
 import java.time.LocalDate;
 
-public class Task {
-	String task1 = "牛乳を買う";
-	String task2 = "○○社面談";
-	String task3 = "手帳を買う";
-	String task4 = "散髪に行く";
-	String task5 = "スクールの課題を解く。";
+public class Task implements Comparable<Task> {
+	private String task;
+	private LocalDate day;
 	
+	public Task() {}
+	
+	public Task(String task, LocalDate day) {
+		this.task = task;
+		this.day = day;
+	}
+	
+	String task1 = "牛乳を買う";
 	LocalDate day1 = LocalDate.of(2021, 10, 21);
+	
+	String task2 = "○○社面談";
 	LocalDate day2 = LocalDate.of(2021, 9, 15);
+	
+	String task3 = "手帳を買う";
 	LocalDate day3 = LocalDate.of(2021, 12, 04);
+
+	String task4 = "散髪に行く";
 	LocalDate day4 = LocalDate.of(2021, 8, 10);
-	LocalDate day5= LocalDate.of(2021, 11, 9);
+	
+	String task5 = "スクールの課題を解く。";
+	LocalDate day5 = LocalDate.of(2021, 11, 9);
+	
+	public String getTask() {
+		return task;
+	}
+	public void setTask(String task) {
+		this.task = task;
+	}
+	public LocalDate getDay() {
+		return this.day;
+	}
+	public void setDay(LocalDate day) {
+		this.day = day;
+	}
+
+	
+	public int compareTo(Task obj) {
+		return this.day.compareTo(obj.day);
+	}
+	
 }
